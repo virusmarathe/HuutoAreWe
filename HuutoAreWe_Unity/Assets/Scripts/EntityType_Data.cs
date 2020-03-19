46b42e7bc5c537771a951561000ad1b59fd13633
virusmarathe <virusmarathe@gmail.com>
2020-03-18 22:32:45 -0700

    starter images and entity structure

---
+++ HuutoAreWe_Unity/Assets/Scripts/EntityType_Data.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EntityTypes", menuName = "ScriptableObjects/EntityTypes", order = 1)]
+public class EntityType_Data : ScriptableObject
+{
+    [System.Serializable]
+    public class EntityTypeStructure
+    {
+        public string Name;
+        public int ID;
+        public Texture2D DefaultSprite;
+    }
+
+    public List<EntityTypeStructure> EntityTypes = new List<EntityTypeStructure>();
+}
