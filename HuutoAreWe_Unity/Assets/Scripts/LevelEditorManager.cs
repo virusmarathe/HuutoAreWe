11750bfdb35f7dba4426751fa8316cf101fd4df5
virusmarathe <virusmarathe@gmail.com>
2020-03-21 17:17:29 -0700

    tile sprite setting completed
    setting tile sprite in editor working

--- HuutoAreWe_Unity/Assets/Scripts/LevelEditorManager.cs
+++ HuutoAreWe_Unity/Assets/Scripts/LevelEditorManager.cs
@@ -9,9 +9,13 @@ public class LevelEditorManager : MonoBehaviour
     public Dropdown EntityDropdown;
     public EntityType_Data EntityData;
 
+    Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
+        cam = Camera.main;
+
         List<Dropdown.OptionData> optionsList = new List<Dropdown.OptionData>();
         foreach(EntityType_Data.EntityTypeStructure ed in EntityData.EntityTypes)
         {
@@ -24,6 +28,24 @@ public class LevelEditorManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 screenPos = cam.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(screenPos, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                EditorTile tile = hit.collider.GetComponent<EditorTile>();
+                if (tile)
+                {
+                    HandleHit(tile);
+                }
+            }
+        }
+    }
+
+    void HandleHit(EditorTile tile)
+    {
+        tile.SetSprite(EntityDropdown.options[EntityDropdown.value].image);
     }
 }
